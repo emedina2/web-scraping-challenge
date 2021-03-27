fc2a8f1d7c6411b68b3b36d82699400ef67f0688
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-03-26 20:04:36 -0500

    Update app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -7,12 +7,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 from flask_pymongo import PyMongo
 
-
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+
+
+
 @app.route("/")
 def home():
-    test_data = mongo.db.collection.find_one()
+    test_data = mongo.db.mars_data.find_one()
     return render_template('index.html', test = test_data)
 
 @app.route("/scrape")
